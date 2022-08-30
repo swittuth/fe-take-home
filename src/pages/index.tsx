443ce4c055b925a70db7b09123a27b9e777d22fb
adri-triana <adritriana26@gmail.com>
2022-08-30 15:06:49 -0400

    Strip excess code out and start setting up basic layout for page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,24 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Text, Flex, useTheme, useColorMode } from "@chakra-ui/react";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Container } from "../components/Container";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import HypeLogo from "../components/HypeLogo";
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text color="text">
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+const Index = () => {
+  const { colorMode } = useColorMode();
 
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+  return (
+    <Container height="100vh">
+      <Flex w={"100%"} h={"80px"} alignItems={"center"} px={4}>
+        <HypeLogo
+          fillColor={colorMode === "dark" ? "white" : "black"}
+          height={30}
+        />
+      </Flex>
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+      <DarkModeSwitch />
+    </Container>
+  );
+};
 
-export default Index
+export default Index;
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,40 +1,52 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from "@chakra-ui/react";
 
-const fonts = { mono: `'Menlo', monospace` }
-
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+const fonts = { mono: `'Menlo', monospace` };
 
 const theme = extendTheme({
+  breakpoints: {
+    sm: "40em",
+    md: "52em",
+    lg: "64em",
+    xl: "80em",
+  },
   semanticTokens: {
     colors: {
-      text: {
-        default: '#16161D',
-        _dark: '#ade3b8',
-      },
-      heroGradientStart: {
-        default: '#7928CA',
-        _dark: '#e3a7f9',
-      },
-      heroGradientEnd: {
-        default: '#FF0080',
-        _dark: '#fbec8f',
+      siteBackground: {
+        default:
+          "linear-gradient(180deg, rgba(152, 164, 230, 0.2) 0%, rgba(217, 217, 255, 0) 31.09%),linear-gradient(0deg, #F8F8FC, #F8F8FC)",
+        _dark: "linear-gradient(180deg, #141624 0%, #090A10 1%)",
       },
     },
     radii: {
-      button: '12px',
+      button: "12px",
     },
   },
   colors: {
-    black: '#16161D',
+    black: "#16161D",
   },
   fonts,
-  breakpoints,
-})
+  styles: {
+    global: {
+      html: {
+        height: "100%",
+        background: "siteBackground",
+      },
+      "#__next": {
+        height: "100%",
+      },
+      body: {
+        padding: 0,
+        margin: 0,
+        height: "100%",
+        width: "100%",
+        background: "siteBackground !important",
+      },
+      a: {
+        color: "inherit",
+        textDecoration: "none",
+      },
+    },
+  },
+});
 
-export default theme
+export default theme;
