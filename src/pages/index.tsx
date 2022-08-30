7eee3068081ab757724abd0f3643df27ad90a68b
adri-triana <adritriana26@gmail.com>
2022-08-30 15:56:01 -0400

    Hook up hyperspace client and install react icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
-import { Text, Flex, useTheme, useColorMode } from "@chakra-ui/react";
+import { Flex, useColorMode } from "@chakra-ui/react";
 
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import HypeLogo from "../components/HypeLogo";
+import { HyperspaceClient } from "hyperspace-client-js";
+
+const clientApiKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBZHJpYW5vIiwibmFtZSI6Ikh5cGVyc3BhY2UiLCJpYXQiOjE1MTYyMzkwMjJ9.NjoCxkNRUKh05tG9i1lacAdi9j6aku_V6ZIA8teOBKg";
 
 const Index = () => {
   const { colorMode } = useColorMode();
+
+  // API client for accessing Hyperspace data
+  const hyperClient = new HyperspaceClient(clientApiKey);
 
   return (
     <Container height="100vh">
@@ -15,6 +22,8 @@ const Index = () => {
           height={30}
         />
       </Flex>
+
+      {/* Using our Hyperspace data, build out something cool for us to review together! */}
 
       <DarkModeSwitch />
     </Container>
