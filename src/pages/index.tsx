9441d71c34e951a9ed66e7f0be84ffc762278d34
adri-triana <adritriana26@gmail.com>
2022-09-07 12:00:53 -0400

    Hard coding client key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,13 @@ import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import HypeLogo from "../components/HypeLogo";
 import { HyperspaceClient } from "hyperspace-client-js";
 
-const clientApiKey = process.env.NEXT_PUBLIC_API_KEY;
-
 const Index = () => {
   const { colorMode } = useColorMode();
 
   // API client for accessing Hyperspace data
-  const hyperClient = new HyperspaceClient(clientApiKey);
+  const hyperClient = new HyperspaceClient(
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJGRSBJbnRlcnZpZXciLCJuYW1lIjoiSHlwZXJzcGFjZSIsImlhdCI6MTUxNjIzOTAyMn0.HDfB97Y1pgQqQ6GshXsh5nz7fA1_ban9MTZDAbgobJk"
+  );
 
   return (
     <Container height="100vh">
