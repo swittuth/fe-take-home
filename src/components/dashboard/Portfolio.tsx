c6f2c3982ed85bf8df3139732290f8cde50335f3
swittuthcoding <swittuthcoding@gmail.com>
2022-09-10 16:28:52 -0400

    updated transaction container

---
+++ src/components/dashboard/Portfolio.tsx
@@ -0,0 +1,4 @@
+import { useContext } from "react";
+import { Line } from "react-chartjs-2";
+
+export const Portfolio = () => {};
--- src/components/dashboard/TransactionListing.tsx
+++ src/components/dashboard/TransactionListing.tsx
@@ -1,9 +1,49 @@
 import { Flex } from "@chakra-ui/react";
+import { useEffect, useContext, useState } from "react";
+import { MarketPlaceActionEnum } from "hyperspace-client-js/dist/sdk";
+import { InfoContext } from "../../infocontext";
+import { Transaction } from "./Transaction";
 
 export const TransactionListing = () => {
-  <Flex
-    direction={"column"}
-  >
+  const { userAddress, hyperClient } = useContext(InfoContext);
+  const [transactions, setTransactions] = useState([]);
 
-  </Flex>
-}
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
+  async function getTransactions() {
+    const userHistory = await hyperClient.getUserHistory({
+      condition: {
+        userAddress: "4ZCiGakZJy5aJsLpMBNBNwyrmNCCSCzukzhaPzzd4d7v",
+        actionTypes: [MarketPlaceActionEnum.Transaction],
+      },
+    });
+    const resultUserHistory = userHistory.getUserHistory;
+    setTransactions(resultUserHistory.market_place_snapshots);
+  }
+
+  return (
+    <Flex
+      background="#171A2799"
+      rounded="lg"
+      direction={"column"}
+      h="88vh"
+      w="100%"
+      padding="10px"
+      gap="10px"
+      overflow={"auto"}
+    >
+      <p>TRANSACTIONS</p>
+      {transactions.map((obj) => (
+        <Transaction
+          key={obj.market_place_state.block_timestamp}
+          project_name={obj.project_name}
+          meta_data_img={obj.meta_data_img}
+          price={obj.market_place_state.price}
+          block_timestamp={obj.market_place_state.block_timestamp}
+        />
+      ))}
+    </Flex>
+  );
+};
