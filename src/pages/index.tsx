afff997267160f103d5ee2ed685fb45ac93e6f2e
swittuthcoding <swittuthcoding@gmail.com>
2022-09-10 10:57:57 -0400

    updated api testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import HypeLogo from "../components/HypeLogo";
 import { HyperspaceClient } from "hyperspace-client-js";
+import {
+  MarketPlaceActionEnum,
+  NonMarketPlaceActionEnum,
+} from "hyperspace-client-js/dist/sdk";
 
 const Index = () => {
   const { colorMode } = useColorMode();
@@ -19,27 +23,51 @@ const Index = () => {
 
   async function getData() {
     // get wallet stats - overall stats specified with the timePeriod attribute value
-    const walletStats = await hyperClient.getWalletStats({
+    // const walletStats = await hyperClient.getWalletStats({
+    //   condition: {
+    //     searchAddress: "4ZCiGakZJy5aJsLpMBNBNwyrmNCCSCzukzhaPzzd4d7v",
+    //     includeUserRank: true,
+    //     timePeriod: "ALL",
+    //   },
+    // });
+    // const resultWallStats = walletStats.getWalletStats;
+    // console.log("res wall stats", resultWallStats);
+
+    const nmHistory = hyperClient.getNonMpaUserHistory({
       condition: {
-        searchAddress: "4ZCiGakZJy5aJsLpMBNBNwyrmNCCSCzukzhaPzzd4d7v",
-        includeUserRank: true,
-        timePeriod: "ALL",
+        userAddress: "DGobS8SQjHGqdf2Yrzq5mq86Vysy7EA8mt6f6tWUtFWe",
+        nonMpaActionTypes: [NonMarketPlaceActionEnum.Mint],
       },
     });
-    // can display loading screen while fetch is being made
-    const resultWallStats = await walletStats.getWalletStats;
-    console.log("res wall stats", resultWallStats);
+    const resNmHistory = (await nmHistory).getNonMpaUserHistory;
+    console.log("nm history", resNmHistory);
 
     // get wallet stat history
     const result = await hyperClient
       .getWalletStatsHist({
         condition: {
           searchAddress: "4ZCiGakZJy5aJsLpMBNBNwyrmNCCSCzukzhaPzzd4d7v",
+          dayLookback: "ONE_DAY",
         },
       })
       .then((response) => response.getWalletStatsHist);
 
-    console.log(result);
+    console.log("wallet stats hist", result);
+
+    const userHistory = await hyperClient.getUserHistory({
+      condition: {
+        userAddress: "4ZCiGakZJy5aJsLpMBNBNwyrmNCCSCzukzhaPzzd4d7v",
+        actionTypes: [
+          MarketPlaceActionEnum.Bid,
+          MarketPlaceActionEnum.Listing,
+          MarketPlaceActionEnum.Transaction,
+        ],
+      },
+    });
+    const resultUserHistory = userHistory.getUserHistory;
+    console.log("user history", resultUserHistory);
+
+    // can also get data from user related to other details - get buyer's address
   }
 
   return (
